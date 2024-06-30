1b4904c6479f9e82637ac3d363db36ac8b79004c
Anushka Mishra <165766043+anush-mish@users.noreply.github.com>
2024-06-30 08:13:10 +0530

    Add files via upload

---
+++ two.js
@@ -0,0 +1,25 @@
+const userId=1234;
+let userName="Anushka";
+var userEmail="anushka123@gmail.com";
+userCity="Noida";
+console.log(userId);
+console.log(userName);
+console.log(userEmail);
+console.log(userCity);
+
+// userId=5678;
+// console.log(userId);
+// Assignment to constant variable.
+
+// here one thing to note is that we use let and const , var a problem with scope so it is not used or least used among
+// the three of them , other values are changeable but the variable defined as const can't be changed......
+
+
+console.log([userId,userName,userEmail,userCity])
+
+// output:-
+// 1234
+// Anushka
+// anushka123@gmail.com
+// Noida
+// [ 1234, 'Anushka', 'anushka123@gmail.com', 'Noida' ]
